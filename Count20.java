05f029afe2413d7ce495203a2e058c20c9c85df6
Kyjorn <hendonmshane@gmail.com>
2025-05-03 23:35:37 -0600

    Update: Count20 to print even numbers from 2 to 20

--- Count20.java
+++ Count20.java
@@ -1,8 +1,8 @@
 public class Count20 {
 
     public static void main(String[] args) {
-        
-        for(int i = 0; i <= 20; i++){
+
+        for(int i = 2; i <= 20; i += 2){
             System.out.println(i);
         }
     }
